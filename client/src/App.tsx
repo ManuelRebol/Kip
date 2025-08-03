c30e6ba5004dc575e5ec9382056ce4040f8fcdb0
ManuelRebol <manuel.rebol0103@gmail.com>
2025-08-03 10:30:27 -0300

    feature: configuración inicial del frontend con vite

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { Footer } from "./components/Footer"
+import { Main } from "./components/Main"
+import { Nav } from "./components/Nav"
+
+export const App = () => {
+  return (
+    <>
+      <Nav />
+      <Main />
+      <Footer />
+    </>
+  )
+}
